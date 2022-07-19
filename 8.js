d3094e10e0b9bbf7af1d4cb96ee5bd2edbeb0863
Nmohd <najeebu50@gmail.com>
2022-07-20 04:36:07 +0530

    done with string concatenation

--- 8.js
+++ 8.js
@@ -1,5 +1,29 @@
 // typeof operator
 
+// Data types (primitive data types)
+//string
+// Numbers
+// booleans
+//undefined
+//null
+//bigint
+//symbol
+
+// let age = 22;
+
 // convert number to string.
+// let age = 22;
+// let firstname = "mohd";
+
+// age = age + "";
+
+// let mynumber = +"34";
+// console.log(typeof mynumber);
+
+// console.log(typeof age);
 
 // convert string to number.
+
+let age = "22";
+age = String(age);
+console.log(typeof age, age);
---
+++ 9.js
@@ -0,0 +1,14 @@
+//string concatenation
+
+// let string1 = "10";
+// let string2 = "20";
+// let newstring = +string1 + +string2;
+// console.log(typeof newstring, newstring);
+
+let firstname = "mohd";
+let midname = "Najeeb";
+let age = 24;
+
+let line = "My name is " + firstname + " " + midname + " " + "my age is " + age;
+
+console.log(line);
