0ef3606eff8f793251773386cdfab6d9707a0396
Nmohd <najeebu50@gmail.com>
2022-07-20 05:04:51 +0530

    added 12.js

---
+++ 10.js
@@ -0,0 +1,10 @@
+//template string
+
+let age = 22;
+let firstname = "mohd";
+
+// let aboutMe = "my name is " + firstname + " and my age is " + age;
+
+let aboutMe = `my name is ${firstname} and my age is ${age}`;
+
+console.log(aboutMe);
